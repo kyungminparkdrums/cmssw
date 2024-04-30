7b20cad305a4915872ff8243312a80f26294bc81
Kyungmin Park <kyungmin.park@cern.ch>
2024-04-30 19:55:44 +0200

    skeleton for hgc multiclass id integration

--- L1Trigger/Phase2L1ParticleFlow/interface/HGC3DClusterID.h
+++ L1Trigger/Phase2L1ParticleFlow/interface/HGC3DClusterID.h
@@ -8,20 +8,41 @@
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 #include "CommonTools/Utils/interface/StringObjectFunction.h"
 
+#include "DataFormats/L1TParticleFlow/interface/layer1_emulator.h"
+//#include "conifer.h"
+#include "conifer_cpp.h"
+
 #include <vector>
 #include <cmath>
+#include <algorithm>
+
+// For conifer model inference
+//typedef ap_fixed<21, 12, AP_RND_CONV, AP_SAT> bdt_feature_t;
+//typedef ap_fixed<12, 3, AP_RND_CONV, AP_SAT> bdt_score_t;
+//conifer::BDT<bdt_feature_t, ap_fixed<12, 3, AP_RND_CONV, AP_SAT>, false> *multiclass_bdt_;
+
+typedef ap_fixed<20, 10> bdt_feature_t;
+typedef ap_fixed<20, 10> bdt_score_t;
+//typedef ap_fixed<18, 8, AP_RND_CONV, AP_SAT> bdt_feature_t;
+//typedef ap_fixed<18, 8, AP_RND_CONV, AP_SAT> bdt_score_t;
+//conifer::BDT<bdt_feature_t, ap_fixed<18, 8, AP_RND_CONV, AP_SAT>, false> *multiclass_bdt_;
+conifer::BDT<bdt_feature_t, bdt_score_t, false> *multiclass_bdt_;
+
+
+std::vector<bdt_feature_t> inputs;
+std::vector<bdt_score_t> bdt_score;
 
 namespace l1tpf {
   class HGC3DClusterID {
   public:
     HGC3DClusterID(const edm::ParameterSet &pset);
 
-    void evaluate(const l1t::HGCalMulticluster &cl, l1t::PFCluster &cpf) const;
+    float evaluate(const l1t::HGCalMulticluster &cl, l1t::PFCluster &cpf);
 
-    bool passPuID(l1t::PFCluster &cpf);
-    bool passPFEmID(l1t::PFCluster &cpf);
-    bool passEgEmID(l1t::PFCluster &cpf);
-    bool passPiID(l1t::PFCluster &cpf);
+    bool passPuID(l1t::PFCluster &cpf, float maxScore);
+    bool passPFEmID(l1t::PFCluster &cpf, float maxScore);
+    bool passEgEmID(l1t::PFCluster &cpf, float maxScore);
+    bool passPiID(l1t::PFCluster &cpf, float maxScore);
 
   private:
     class Var {
