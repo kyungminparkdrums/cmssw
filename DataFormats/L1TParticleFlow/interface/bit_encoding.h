1dd1a81d42d60dcf2f3d29e168c9e4df415c1f73
Gianluca <gianluca.cerminara@cern.ch>
2025-02-10 12:56:45 +0100

    equip for endcap barrel dedicated packing and unpacking of calo objs

--- DataFormats/L1TParticleFlow/interface/bit_encoding.h
+++ DataFormats/L1TParticleFlow/interface/bit_encoding.h
@@ -28,63 +28,107 @@ inline void unpack_bool_from_bits(const U& u, unsigned int& start, bool& data) {
   data = u[start++];
 }
 
-template <unsigned int N, unsigned int OFFS = 0, typename T, int NB>
+enum class PackingStrategy { DEFAULT, BARREL, ENDCAP };
+
+// Helper function that calls the correct unpack method based on user input
+template <typename T, int NB, PackingStrategy METHOD = PackingStrategy::DEFAULT>
+inline auto unpack_helper(const ap_uint<NB>& data) {
+  if constexpr (METHOD == PackingStrategy::BARREL) {
+    static_assert(T::BITWIDTH_BARREL <= NB, "NB Type is too small for the object");
+    return T::unpack_barrel(data);
+  } else if constexpr (METHOD == PackingStrategy::ENDCAP) {
+    static_assert(T::BITWIDTH_ENDCAP <= NB, "NB Type is too small for the object");
+    return T::unpack_endcap(data);
+  } else {
+    static_assert(T::BITWIDTH <= NB, "NB Type is too small for the object");
+    return T::unpack(data);
+  }
+}
+
+template <typename T, int NB, PackingStrategy METHOD = PackingStrategy::DEFAULT>
+inline auto pack_helper(const T& obj) {
+  if constexpr (METHOD == PackingStrategy::BARREL) {
+    static_assert(T::BITWIDTH_BARREL <= NB, "NB Type is too small for the object");
+
+    return obj.pack_barrel();
+  } else if constexpr (METHOD == PackingStrategy::ENDCAP) {
+    static_assert(T::BITWIDTH_ENDCAP <= NB, "NB Type is too small for the object");
+
+    return obj.pack_endcap();
+  } else {
+    static_assert(T::BITWIDTH <= NB, "NB Type is too small for the object");
+
+    return obj.pack();
+  }
+}
+
+template <typename T, int NB, PackingStrategy METHOD>
+inline auto pack_slim_helper(const T& obj) {
+  if constexpr (METHOD == PackingStrategy::BARREL) {
+    static_assert(T::BITWIDTH_BARREL_SLIM <= NB, "NB Type is too small for the object");
+    return obj.pack_barrel_slim();
+  } else if constexpr (METHOD == PackingStrategy::ENDCAP) {
+    static_assert(T::BITWIDTH_ENDCAP_SLIM <= NB, "NB Type is too small for the object");
+    return obj.pack_endcap_slim();
+  } else {
+    static_assert(T::BITWIDTH_SLIM <= NB, "NB Type is too small for the object");
+    return obj.pack_slim();
+  }
+}
+
+template <unsigned int N, PackingStrategy METHOD = PackingStrategy::DEFAULT, unsigned int OFFS = 0, typename T, int NB>
 inline void l1pf_pattern_pack(const T objs[N], ap_uint<NB> data[]) {
 #ifdef __SYNTHESIS__
 #pragma HLS inline
 #pragma HLS inline region recursive
 #endif
-  assert(T::BITWIDTH <= NB);
   for (unsigned int i = 0; i < N; ++i) {
 #ifdef __SYNTHESIS__
 #pragma HLS unroll
 #endif
-    data[i + OFFS] = objs[i].pack();
+    data[i + OFFS] = pack_helper<T, NB, METHOD>(objs[i]);
   }
 }
 
-template <unsigned int N, unsigned int OFFS = 0, typename T, int NB>
+template <unsigned int N, PackingStrategy METHOD = PackingStrategy::DEFAULT, unsigned int OFFS = 0, typename T, int NB>
 inline void l1pf_pattern_unpack(const ap_uint<NB> data[], T objs[N]) {
 #ifdef __SYNTHESIS__
 #pragma HLS inline
 #pragma HLS inline region recursive
 #endif
-  assert(T::BITWIDTH <= NB);
   for (unsigned int i = 0; i < N; ++i) {
 #ifdef __SYNTHESIS__
 #pragma HLS unroll
 #endif
-    objs[i] = T::unpack(data[i + OFFS]);
+    objs[i] = unpack_helper<T, NB, METHOD>(data[i + OFFS]);
   }
 }
 
-template <unsigned int N, unsigned int OFFS = 0, typename T, int NB>
+template <unsigned int N, PackingStrategy METHOD = PackingStrategy::DEFAULT, unsigned int OFFS = 0, typename T, int NB>
 inline void l1pf_pattern_pack_slim(const T objs[N], ap_uint<NB> data[]) {
 #ifdef __SYNTHESIS__
 #pragma HLS inline
 #pragma HLS inline region recursive
 #endif
-  assert(T::BITWIDTH_SLIM <= NB);
   for (unsigned int i = 0; i < N; ++i) {
 #ifdef __SYNTHESIS__
 #pragma HLS unroll
 #endif
-    data[i + OFFS] = objs[i].pack_slim();
+    data[i + OFFS] = pack_slim_helper<T, NB, METHOD>(objs[i]);
   }
 }
 
-template <unsigned int N, unsigned int OFFS = 0, typename T, int NB>
+template <unsigned int N, PackingStrategy METHOD = PackingStrategy::DEFAULT, unsigned int OFFS = 0, typename T, int NB>
 inline void l1pf_pattern_unpack_slim(const ap_uint<NB> data[], T objs[N]) {
 #ifdef __SYNTHESIS__
 #pragma HLS inline
 #pragma HLS inline region recursive
 #endif
-  assert(T::BITWIDTH_SLIM <= NB);
   for (unsigned int i = 0; i < N; ++i) {
 #ifdef __SYNTHESIS__
 #pragma HLS unroll
 #endif
-    objs[i] = T::unpack(data[i + OFFS]);
+    objs[i] = unpack_helper<T, NB, METHOD>(data[i + OFFS]);
   }
 }
 
